b98e6761bbd3a61ca296e26746445a7a7585cdd3
Shardendu Mishra <shardendumishra01@gmail.com>
2024-11-13 15:12:04 +0530

    Java 1st Program Ever

--- Basic One/HelloWorld_1.java
+++ Basic One/HelloWorld_1.java
@@ -1,5 +1,6 @@
-public class HelloWorld {
+public class HelloWorld_1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This is my first Program");
     }
 }
