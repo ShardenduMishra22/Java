dabeeaf1726c3adaad02fc3e748ba9295e9e05ee
Shardendu Mishra <shardendumishra01@gmail.com>
2024-11-13 18:48:57 +0530

    Learning Java

---
+++ Basic_4/MathExample.java
@@ -0,0 +1,11 @@
+package Basic_4;
+
+public class MathExample {
+    public static void main(String args[]) {
+        int n1 = 10;
+        int n2 = 20;
+
+        int max = Math.max(n1, n2);
+        System.out.println("Max value: " + max);
+    }
+}
